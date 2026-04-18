7f788e3cab45a05552228a3c72c93c60ac427f2b
Mahendra Mahawar <mahendramahawar@Mahendras-MacBook-Air.local>
2026-04-19 00:28:58 +0530

    added tailwind config new 5

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,14 +1,17 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    
-    // 👇 VERY IMPORTANT
-    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "../../packages/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  
 };
+
+export default config;
