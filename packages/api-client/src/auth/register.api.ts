f6c02958de2b53d929d7cbfbda06cefe46c5d2a2
Mahendra Mahawar <mahendramahawar@Mahendras-MacBook-Air.local>
2026-06-04 18:21:55 +0530

    done remove add node modules

--- packages/api-client/src/auth/register.api.ts
+++ packages/api-client/src/auth/register.api.ts
@@ -6,6 +6,7 @@ export async function registerApi(
   payload: RegisterInput,
 ): Promise<ApiResponse<RegisterUserResponse>> {
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
   if (!API_BASE_URL) {
     throw new Error("NEXT_PUBLIC_API_URL is missing");
   }
